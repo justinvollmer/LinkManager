bc833f50e6f1b9a17245209086cabe59b0140ad1
Justin Vollmer <89389760+Jvst1n01@users.noreply.github.com>
2022-11-09 18:39:56 +0100

    DownloadManager UPDATED

--- src/app/DownloadManager.java
+++ src/app/DownloadManager.java
@@ -5,13 +5,45 @@ import java.net.URL;
 
 public class DownloadManager {
     private String path;
+    private String enforcedNamingSystem;
 
-    public DownloadManager (String path) {
+    public DownloadManager (String path, String enforcedNamingSystem) {
         this.path = path;
+        this.enforcedNamingSystem = enforcedNamingSystem;
     }
 
-    public static void downloadImage(String linkToImage, String path, String filename, String filetype) throws IOException {
-        URL url = new URL(linkToImage);
+    public void downloadMedia(String linkToMedia, int imageNumber) throws IOException {
+        String filetype;
+        if (linkToMedia.contains(".jpg")) {
+            filetype = ".jpg";
+        } else if (linkToMedia.contains(".png")) {
+            filetype = ".png";
+        } else if (linkToMedia.contains(".gif")) {
+            filetype = ".gif";
+        } else if (linkToMedia.contains(".mp4")) {
+            filetype = ".mp4";
+        } else if(linkToMedia.contains(".mp3")) {
+            filetype = ".mp3";
+        } else if(linkToMedia.contains(".webm")) {
+            filetype = ".webm";
+        } else if(linkToMedia.contains(".webp")) {
+            filetype = ".jpg";
+        } else {
+            filetype = ".jpg";
+        }
+        URL url = new URL(linkToMedia);
+        InputStream in = new BufferedInputStream(url.openStream());
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(path + this.enforcedNamingSystem + "_" + imageNumber + filetype));
+
+        for ( int i; (i = in.read()) != -1; ) {
+            out.write(i);
+        }
+        in.close();
+        out.close();
+    }
+
+    public static void downloadMedia(String linkToMedia, String path, String filename, String filetype) throws IOException {
+        URL url = new URL(linkToMedia);
         InputStream in = new BufferedInputStream(url.openStream());
         OutputStream out = new BufferedOutputStream(new FileOutputStream(path + filename + filetype));
 
