887635bd67801773eb3e0ca5fbaf3bf13c601730
Justin Vollmer <89389760+Jvst1n01@users.noreply.github.com>
2022-11-17 22:52:19 +0100

    ADDED Encryption class + Version updated

--- src/app/Version.java
+++ src/app/Version.java
@@ -4,7 +4,7 @@ public class Version {
     private static String softwareState = "beta";
     private static int majorVersion = 1;
     private static int minorVersion = 2;
-    private static int patchVersion = 0;
+    private static int patchVersion = 1;
 
     public static String getSoftwareState() {
         return softwareState.toUpperCase();
