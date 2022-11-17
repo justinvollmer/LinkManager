5108ba7148bad29ddf535661438f97a4dd773e29
Justin Vollmer <89389760+Jvst1n01@users.noreply.github.com>
2022-11-17 22:50:35 +0100

    ADDED Encryption class

---
+++ src/app/StrongAES.java
@@ -0,0 +1,31 @@
+package app;
+import java.security.Key;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+public class StrongAES
+{
+    public void run()
+    {
+        try
+        {
+            String text = "Hello World";
+            String key = "PUT KEY HERE"; // 128 bit key
+            // Create key and cipher
+            Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            // encrypt the text
+            cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+            byte[] encrypted = cipher.doFinal(text.getBytes());
+            System.err.println(new String(encrypted));
+            // decrypt the text
+            cipher.init(Cipher.DECRYPT_MODE, aesKey);
+            String decrypted = new String(cipher.doFinal(encrypted));
+            System.err.println(decrypted);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
