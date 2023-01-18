ca5dcd99d6fe029bc282d3c62e95026f64cb33f6
Justin Vollmer <justinvollmer01@googlemail.com>
2023-01-18 01:58:55 +0100

    UPDATED Version

--- src/app/Version.java
+++ src/app/Version.java
@@ -3,7 +3,7 @@ package app;
 public class Version {
     private static int majorVersion = 1;
     private static int minorVersion = 2;
-    private static int patchVersion = 0;
+    private static int patchVersion = 1;
 
     public static String getSoftwareVersion() {
         return "v" + majorVersion + "." + minorVersion + "." + patchVersion;
